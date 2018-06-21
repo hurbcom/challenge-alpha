276817484391b01a4b23922a9cb26f07ba14f863
Loubake <loubake@hotmail.com>
2018-06-21 02:45:10 -0300

    Adicionando arquivos iniciais do projeto.

---
+++ challenge/app/src/main/java/br/com/loubake/challenge_hu/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.loubake.challenge_hu
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
