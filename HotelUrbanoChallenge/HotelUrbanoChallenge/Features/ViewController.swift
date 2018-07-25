f8084f957bafcc80fcab67243aa6f0d06cf9457f
Ronilson Batista <ronilson.batista@orama.com.br>
2018-07-25 14:36:05 -0300

    organization of the project and installation of libs

---
+++ HotelUrbanoChallenge/HotelUrbanoChallenge/Features/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HotelUrbanoChallenge
+//
+//  Created by maciosdev on 25/07/2018.
+//  Copyright © 2018 Ronilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
