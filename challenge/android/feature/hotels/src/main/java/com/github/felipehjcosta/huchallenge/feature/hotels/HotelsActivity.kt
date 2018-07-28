6036125582ce8857b4751e276b584c0a2adae2e4
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-07-28 16:04:05 -0300

    Project setup

---
+++ challenge/android/feature/hotels/src/main/java/com/github/felipehjcosta/huchallenge/feature/hotels/HotelsActivity.kt
@@ -0,0 +1,12 @@
+package com.github.felipehjcosta.huchallenge.feature.hotels
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HotelsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hotels_activity)
+    }
+}
