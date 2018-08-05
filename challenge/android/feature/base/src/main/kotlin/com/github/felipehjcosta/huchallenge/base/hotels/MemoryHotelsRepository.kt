679b16f3b42b866828d9daf35b953b2aa24df4b7
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-08-05 01:20:23 -0300

    Remove old memory repository

--- challenge/android/feature/base/src/main/kotlin/com/github/felipehjcosta/huchallenge/base/hotels/MemoryHotelsRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.github.felipehjcosta.huchallenge.base.hotels
-
-import io.reactivex.Observable
-
-internal class MemoryHotelsRepository : HotelsRepository {
-    override fun fetchHotels(): Observable<List<String>> {
-        return Observable.just(listOf("Hotel Vilamar Copacabana"))
-    }
-}
