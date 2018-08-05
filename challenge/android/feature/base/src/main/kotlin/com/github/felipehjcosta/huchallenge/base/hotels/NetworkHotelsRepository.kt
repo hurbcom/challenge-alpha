645c92c4a7ff27db112c26b4dd76673b3b9d8aee
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-08-05 01:20:53 -0300

    Make NetworkHotelsRepository internal of base module

--- challenge/android/feature/base/src/main/kotlin/com/github/felipehjcosta/huchallenge/base/hotels/NetworkHotelsRepository.kt
+++ challenge/android/feature/base/src/main/kotlin/com/github/felipehjcosta/huchallenge/base/hotels/NetworkHotelsRepository.kt
@@ -2,7 +2,7 @@ package com.github.felipehjcosta.huchallenge.base.hotels
 
 import io.reactivex.Observable
 
-class NetworkHotelsRepository(private val searchApi: SearchApi) : HotelsRepository {
+internal class NetworkHotelsRepository(private val searchApi: SearchApi) : HotelsRepository {
 
     override fun fetchHotels(): Observable<List<String>> {
         return searchApi.searchHotels().map { it.results.map { it.name } }
