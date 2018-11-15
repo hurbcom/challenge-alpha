5f569dc086b5fbcf46244f283fb5c2f0046e82f5
gusjasponde <gustavocalonico@gmail.com>
2018-11-15 02:50:02 -0200

    Initial commit :tada:

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const HomeStack = createStackNavigator({
+  Links: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+    />
+  ),
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  ),
+};
+
+export default createDrawerNavigator({
+  HomeStack,
+  SettingsStack,
+});
