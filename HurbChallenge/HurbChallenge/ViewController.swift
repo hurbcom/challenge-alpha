0e702c8238c957bc70007947e905ffc88b414f0a
Felipe Alves <felipe@bolzanialves.com.br>
2019-07-05 20:12:22 -0300

    Criando projeto inicial

---
+++ HurbChallenge/HurbChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HurbChallenge
+//
+//  Created by Felipe Alves on 05/07/19.
+//  Copyright © 2019 Bolzaniapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HurbChallenge/HurbChallengeTests/HurbChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  HurbChallengeTests.swift
+//  HurbChallengeTests
+//
+//  Created by Felipe Alves on 05/07/19.
+//  Copyright © 2019 Bolzaniapps. All rights reserved.
+//
+
+import XCTest
+@testable import HurbChallenge
+
+class HurbChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
