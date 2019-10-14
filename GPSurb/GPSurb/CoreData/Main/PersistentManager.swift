74fe37020bec0df6bd47cfd5d1ea12127c01981a
Gilson Santos <gilsonsantosti@gmail.com>
2019-10-14 11:11:41 -0300

    Criação da estrutura base do projeto.

---
+++ GPSurb/GPSurb/CoreData/Main/PersistentManager.swift
@@ -0,0 +1,14 @@
+//
+//  PersistentManager.swift
+//  GPSurb
+//
+//  Created by Gilson Santos on 14/10/19.
+//  Copyright © 2019 Gilson Santos. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class PersistentManager {
+    
+}
