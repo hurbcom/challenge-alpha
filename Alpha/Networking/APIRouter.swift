bb8bdd6d4acafbd499528a086ed48faf2d367558
Theo Mendes <theodefranca@gmail.com>
2019-10-17 10:56:48 -0300

    :bulb: Documented API Router

--- Alpha/Networking/APIRouter.swift
+++ Alpha/Networking/APIRouter.swift
@@ -8,22 +8,31 @@
 
 import Alamofire
 
+/// A Router for the API's endpoints
 enum APIRouter: URLRequestConvertible {
+    /// A search case
     case search(parameters: SearchParams)
-
+    
+    /// The methods for each of the cases
     private var method: HTTPMethod {
         switch self {
         case .search:
             return .get
         }
     }
-
+    /// The paths of the API for each of the cases
     private var path: String {
         switch self {
         case .search:
             return "/search/api"
         }
     }
+
+    /**
+    Create the URL Request based in the method and path
+
+    - Returns: A new `URLRequest`.
+    */
 
     func asURLRequest() throws -> URLRequest {
         let baseURL = try Environment.apiBaseURL.asURL()
