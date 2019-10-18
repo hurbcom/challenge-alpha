a0cfa430af95bd173d76779a0688fbeb9b549800
Theo Mendes <ghostcitizen@protonmail.ch>
2019-10-18 11:38:41 -0300

    :bulb: Documented ViewModelType

--- Alpha/Common/Protocols/ViewModelType.swift
+++ Alpha/Common/Protocols/ViewModelType.swift
@@ -6,11 +6,18 @@
 //  Copyright © 2019 Hurb. All rights reserved.
 //
 
-import Foundation
-
+/// Protocol that all ViewModels must conform to
 protocol ViewModelType {
+    /// Events consumed by the View Model
     associatedtype Input
+    /// Output streams created by the view model
     associatedtype Output
+    /**
+    Transform all the inputs into outputs
 
+    - Parameter input: All the inputs required by the view model
+
+    - Returns: The ViewModel's outputs.
+    */
     func transform(input: Input) -> Output
 }
