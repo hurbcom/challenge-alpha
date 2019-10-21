6751345648a295fb3b471d50b96131384a899d56
Theo Mendes <ghostcitizen@protonmail.ch>
2019-10-21 00:53:45 -0300

    :bulb: Documented Coordinator protocol

--- Alpha/Common/Protocols/Coordinator.swift
+++ Alpha/Common/Protocols/Coordinator.swift
@@ -6,17 +6,28 @@
 //  Copyright © 2019 Hurb. All rights reserved.
 //
 
+/// Protocol that all Coordinators must conform to
 protocol Coordinator: class {
+    /// Child Coordinators
     var childCoordinators: [Coordinator] { get set }
+    /// Start Coordinator
     func start()
 }
 
 extension Coordinator {
+    /**
+    Adds a coordinator to the child coordinators stack
 
+    - Parameter coordinator: The coordinator that should be added
+    */
     func store(coordinator: Coordinator) {
         childCoordinators.append(coordinator)
     }
+    /**
+    Removes a coordinator from the stack
 
+    - Parameter coordinator: The coordinator that should be removed
+    */
     func free(coordinator: Coordinator) {
         childCoordinators = childCoordinators.filter { $0 !== coordinator }
     }
