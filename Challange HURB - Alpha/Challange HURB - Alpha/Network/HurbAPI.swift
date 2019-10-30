a53c41b4b6b5c197852af5229658c5b5b250b437
Luiz Fernando <luizduarte@aluno.puc-rio.br>
2019-10-29 22:34:51 -0300

    :sparkles: Improving network layer
    Now it is possible to make queries from the API

--- Challange HURB - Alpha/Challange HURB - Alpha/Network/HurbAPI.swift
+++ Challange HURB - Alpha/Challange HURB - Alpha/Network/HurbAPI.swift
@@ -27,7 +27,7 @@ extension HurbAPI: TargetType {
     var path: String {
         switch self {
         case .search:
-            return "/search/api"
+            return "/search/api?"
         }
     }
     
