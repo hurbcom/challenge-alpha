c9f3a8de69df9a2952811d594619f2733e33eca9
Luiz Fernando <luizduarte@aluno.puc-rio.br>
2019-11-05 22:29:49 -0300

    :art: Created DataManager
    DataManager is responsible to manage the data received from the API, dividing into two categories: hotels and packages

---
+++ Challange HURB - Alpha/Challange HURB - AlphaTests/DataManagerTests.swift
@@ -0,0 +1,9 @@
+//
+//  DataManagerTests.swift
+//  Challange HURB - AlphaTests
+//
+//  Created by Luiz Fernando Cunha Duarte on 05/11/19.
+//  Copyright © 2019 Luiz Fernando Cunha Duarte. All rights reserved.
+//
+
+import Foundation
