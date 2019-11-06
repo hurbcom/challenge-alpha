584bd1d00711f7814413f3a1c833098d10874829
Luiz Fernando <luizduarte@aluno.puc-rio.br>
2019-11-06 18:59:01 -0300

    :sparkles: Implemented DataManager
    DataManager is responsible for treating the data received from the API

---
+++ Challange HURB - Alpha/Challange HURB - Alpha/Model/DataManager.swift
@@ -0,0 +1,43 @@
+//
+//  DAO.swift
+//  Challange HURB - Alpha
+//
+//  Created by Luiz Fernando Cunha Duarte on 05/11/19.
+//  Copyright © 2019 Luiz Fernando Cunha Duarte. All rights reserved.
+//
+
+import Foundation
+
+/// Class resposible to treat the data received from the API
+class DataManager {
+ 
+    fileprivate var networkAdapter = NetworkAdapter()
+    
+    var experiences: [Experience] = []
+    
+    var hotels: [Experience] {
+        experiences.lazy.filter({ ($0.isHotel ?? false) == true }).sorted(by: >)
+    }
+    
+    var packages: [Experience] {
+        experiences.lazy.filter({ ($0.isPackage ?? false) == true })
+    }
+    
+    init() {
+       getExperiences()
+    }
+    
+    private func getExperiences() {
+        self.networkAdapter.getAPIRespose { (result) in
+            switch result {
+            case .success(let apiResponse):
+                self.experiences = apiResponse.results
+            case .failure(let error):
+                debugPrint("Error in the API query: \(error)")
+                // Error log
+                // Inform Coordinator
+            }
+        }
+    }
+    
+}
