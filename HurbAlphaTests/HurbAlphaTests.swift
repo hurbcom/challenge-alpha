b8090e650944dcc80a6ad3c24919be556c5aa6c7
rodrigomalhaesbuko <rodrigomalhaes@aluno.puc-rio.br>
2019-11-08 16:00:51 -0300

    :white_check_mark: Unit test implemented
    A simple Unit test to cover the get response from the Hurb API

--- HurbAlphaTests/HurbAlphaTests.swift
+++ HurbAlphaTests/HurbAlphaTests.swift
@@ -9,26 +9,44 @@
 import XCTest
 @testable import HurbAlpha
 
-class HurbAlphaUnitTests: XCTestCase {
-
+class HurbAlphaTests: XCTestCase {
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
+    
+    /// Test the get request from API with the key and query requested
+    ///
+    /// Needs internet connection to happen 
+    func testGetFromAPI() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let expectation = self.expectation(description: "get request")
+        
+        netManager.getOffers(place: "buzios", page: 1) { (offers, error) in
+            if error != nil {
+                XCTFail("Couldnt make the request")
+            } else {
+                guard let _ = offers else {
+                    XCTFail("Wrong type of Data ")
+                    return
+                }
+                expectation.fulfill()
+            }
+        }
+        // timeout fro the request
+        self.waitForExpectations(timeout: 10.0)
     }
-
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
