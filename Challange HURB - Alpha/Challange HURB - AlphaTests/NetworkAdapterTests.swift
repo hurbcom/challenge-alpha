e8fbba3df6feb28e2b7138c5f263abe089184b64
Luiz Fernando <luizduarte@aluno.puc-rio.br>
2019-11-12 16:45:30 -0300

    :zap: Improved NetworkAdapterTests

--- Challange HURB - Alpha/Challange HURB - AlphaTests/NetworkAdapterTests.swift
+++ Challange HURB - Alpha/Challange HURB - AlphaTests/NetworkAdapterTests.swift
@@ -2,6 +2,7 @@
 
 import XCTest
 import Moya
+import os.log
 @testable import Challange_HURB___Alpha
 
 class NetworkAdapterTests: XCTestCase {
@@ -9,34 +10,29 @@ class NetworkAdapterTests: XCTestCase {
     var networkAdapter: NetworkAdapter?
     
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        networkAdapter = NetworkAdapter()
+        networkAdapter = NetworkAdapter(state: .test)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        let expectation = XCTestExpectation(description: "Get api response")
-
-        networkAdapter?.getAPIRespose(completion: { (result) in
+    
+    /// Tests if the networkAdapter can handle the query and transform it in an APIResponse
+    func testNetworkLayerAPIResponse() {
+        var expectations: [XCTestExpectation] = []
+        let expectation = XCTestExpectation(description: "Get api response \(page)")
+        expectations.append(expectation)
+        networkAdapter?.getAPIRespose(query: "buzios", page: page, completion: { (result) in
             switch result {
             case .success(let apiResponse):
+                os_log(.info, "Data received from the api successfully")
                 dump(apiResponse)
-                expectation.fulfill()
+                expectations[page-1].fulfill()
             case .failure(let error):
-                debugPrint(error)
+                os_log(.error, "Error receiving Data from the API")
+                dump(error)
             }
         })
-        wait(for: [expectation], timeout: 10.0)
+        wait(for: expectations, timeout: 10.0)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
