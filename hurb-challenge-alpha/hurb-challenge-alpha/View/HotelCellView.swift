22115d46538b9811fe2241529cfb4e8576073109
Hannah Lisboa <hannahlisboa@outlook.com>
2019-12-26 22:29:44 -0300

    add HotelCell and HotelCellViewModel to display hotel or package data in the list

---
+++ hurb-challenge-alpha/hurb-challenge-alpha/View/HotelCellView.swift
@@ -0,0 +1,112 @@
+//
+//  HotelCellView.swift
+//  hurb-challenge-alpha
+//
+//  Created by Hannah  on 26/12/2019.
+//  Copyright © 2019 Hannah . All rights reserved.
+//
+
+import SwiftUI
+
+struct HotelCellView: View {
+    
+    @ObservedObject var cellVM: HotelCellViewModel
+    
+    init(hotel: Accommodation) {
+        cellVM = HotelCellViewModel(hotel: hotel)
+    }
+    
+    var body: some View {
+        
+        ZStack {
+            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                .fill(Color.white).shadow(color: .gray, radius: 1, x: 0, y: 0).padding(2)
+            
+            VStack(alignment: .leading, spacing: 16.0) {
+                VStack {
+                    CustomImageView(url: self.cellVM.urlImage) .overlay(
+                        DiscountView(text: self.cellVM.discount),
+                        alignment: .topTrailing)
+                }.cornerRadius(10).padding(EdgeInsets(top: 0, leading: 1, bottom: 0, trailing: 1))
+                
+                VStack(alignment: .leading, spacing: 5.0) {
+                    Text(self.cellVM.name)
+                        .font(.headline)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .lineLimit(2)
+                    Text(self.cellVM.city)
+                        .font(.headline)
+                        .lineLimit(1)
+                        .foregroundColor(.secondary)
+                        .frame(height: 30, alignment: .top)
+                    CancellationView(isCancellation: self.cellVM.huFreeCancellation)
+                    PriceView(price: self.cellVM.price)
+                }.frame(height: 130)
+                    .padding(10)
+                
+            }
+        }
+    }
+    struct PriceView: View {
+        var price: String
+        var body: some View {
+            VStack {
+                if !self.price.isEmpty {
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text(price)
+                            .foregroundColor(Color.init(hex: "#F2732C")) .font(.headline).fontWeight(.black)
+                            .foregroundColor(.primary)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                        
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+    
+     struct CancellationView: View {
+            var isCancellation: Bool
+            var body: some View {
+                VStack {
+                    if self.isCancellation {
+                     Text("Cancelamento Grátis")
+                                          .font(.body)
+                                          .foregroundColor(.green)
+                                          .lineLimit(1)
+                        
+                    }
+                }
+                
+            }
+        }
+    }
+    
+    struct DiscountView: View {
+        var text: String
+        var body: some View {
+            VStack {
+                if !self.text.isEmpty {
+                    VStack {
+                        Text(text)
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }.padding(8)
+                        .background(Color.init(hex: "#F2732C"))
+                    
+                }
+            }
+            
+        }
+    }
+struct HotelCellView_Previews: PreviewProvider {
+    
+    @State static var  hotelTest = DataContants.sharedInstance.hotelModelTest
+    
+    static var previews: some View {
+        HotelCellView(hotel: hotelTest)
+    }
+}
---
+++ hurb-challenge-alpha/hurb-challenge-alpha/ViewModel/HotelCellViewModel.swift
@@ -0,0 +1,109 @@
+//
+//  HotelCellViewModel.swift
+//  hurb-challenge-alpha
+//
+//  Created by Hannah  on 26/12/2019.
+//  Copyright © 2019 Hannah . All rights reserved.
+//
+
+import SwiftUI
+
+class HotelCellViewModel: ObservableObject {
+    
+    @Published var hotel: Accommodation
+    @Published var discount = String()
+    @Published var price = String()
+    @Published var urlImage = String()
+    @Published var name = String()
+    @Published var city = String()
+    @Published var huFreeCancellation = false
+
+    init(hotel: Accommodation) {
+        self.hotel = hotel
+        self.calculateDiscount()
+        self.loadPrice()
+        self.getUrlImage()
+        self.getCity()
+        self.getName()
+    }
+    
+    // MARK: - Methods
+    /// init huFreeCancellation value
+    fileprivate func getFreeCancelation() {
+           
+        if let huFreeCancellation = self.hotel.huFreeCancellation {
+               self.huFreeCancellation = huFreeCancellation
+           }
+       }
+    
+    // MARK: - Methods
+    /// init name value
+    fileprivate func getName() {
+        
+        if let name = self.hotel.name {
+            self.name = name
+        }
+    }
+    
+    // MARK: - Methods
+    /// init city value
+    fileprivate func getCity() {
+        if let city = self.hotel.address?.city {
+            self.city = city
+        }
+    }
+    
+    // MARK: - Methods
+    ///load value url to Image
+    fileprivate func getUrlImage() {
+        if let url = self.hotel.image {
+            self.urlImage = url
+        } else {
+            if let gallery = self.hotel.gallery {
+                if !gallery.isEmpty {
+                    let image = gallery.first
+                    if let url = image?.url {
+                        self.urlImage = url
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    // MARK: - Methods
+    /// load string price and currency
+    fileprivate func loadPrice() {
+        
+        if let price = self.hotel.price {
+            let currentPrice = price.amount
+            if let currentPrice = currentPrice {
+                let priceString = "R$" + String(Int(currentPrice))
+                self.price = priceString
+            }
+        }
+    }
+    
+    // MARK: - Methods
+    ///calculate discount in accomodation
+    fileprivate func calculateDiscount() {
+        
+        if let price = self.hotel.price {
+            
+            let oldPrice = price.oldPrice
+            let currentPrice = price.amount
+            if let oldPrice = oldPrice {
+                if let currentPrice = currentPrice {
+                    if oldPrice > currentPrice {
+                        let fractionalProgress = ((oldPrice - currentPrice) / oldPrice) * 100
+                        let formatString = "- " + String(Int(fractionalProgress)) + "%"
+                        self.discount = formatString
+                    }
+                }
+            }
+        }
+        
+    }
+    
+}
+
