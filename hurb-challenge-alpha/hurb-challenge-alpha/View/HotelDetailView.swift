c8108b46abb1ffe73522ff87039d51f52d6a73c3
Hannah Lisboa <hannahlisboa@outlook.com>
2019-12-27 00:20:11 -0300

    add HomePageView from load navigation bar

---
+++ hurb-challenge-alpha/hurb-challenge-alpha/View/HotelDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  HotelDetailView.swift
+//  hurb-challenge-alpha
+//
+//  Created by Hannah  on 26/12/2019.
+//  Copyright © 2019 Hannah . All rights reserved.
+//
+
+import SwiftUI
+
+struct HotelDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HotelDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        HotelDetailView()
+    }
+}
