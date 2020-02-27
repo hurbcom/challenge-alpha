a7e2ff9e0e54336a06f2d81f571cd715b84d7fb2
matheus.lima <matheus.lima@maximasistemas.com.br>
2020-02-27 20:53:41 -0300

    implementing subtitle list description (WIP)

---
+++ HurpApp/app/src/main/java/app/recrutamento/android/challengealpha/model/CategoryAmenitySubtitle.kt
@@ -0,0 +1,8 @@
+package app.recrutamento.android.challengealpha.model
+
+import app.recrutamento.android.challengealpha.model.enums.AmenityEnum
+
+data class CategoryAmenitySubtitle (
+    var amenityEnum: AmenityEnum,
+    var amenityIcon: AmenityIcon
+)
