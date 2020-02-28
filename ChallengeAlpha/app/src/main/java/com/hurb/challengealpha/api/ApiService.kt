a167d56610f64c24d7cd58060bc37787d46cf371
Alexsander Franco <alexsanderfrancorosa@gmail.com>
2020-02-28 18:17:39 -0300

    Add API

---
+++ ChallengeAlpha/app/src/main/java/com/hurb/challengealpha/api/ApiService.kt
@@ -0,0 +1,18 @@
+package com.hurb.challengealpha.api
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+val gson: Gson = GsonBuilder().create()
+
+fun getApi(): HurbApi = Retrofit.Builder()
+    .baseUrl(HurbApi.API_URL)
+    .client(OkHttpClient.Builder().build())
+    .addConverterFactory(GsonConverterFactory.create(gson))
+    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+    .build()
+    .create(HurbApi::class.java)
---
+++ ChallengeAlpha/app/src/main/java/com/hurb/challengealpha/api/HurbApi.kt
@@ -0,0 +1,21 @@
+package com.hurb.challengealpha.api
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface HurbApi {
+    companion object {
+        const val API_URL = "https://www.hurb.com/search/api"
+
+        private const val QUERY_PARAM = "q"
+        private const val PAGE_PARAM = "page"
+    }
+
+    @GET
+    fun search(@Query(QUERY_PARAM) q: String, @Query(PAGE_PARAM) page: Int)
+
+    @GET("suggestion")
+    fun suggestion(@Query(QUERY_PARAM) q: String)
+
+
+}
