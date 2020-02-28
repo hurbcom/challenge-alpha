f213d94bf442902c1bf39e322fddb2c8e0b07f48
Alexsander Franco <alexsanderfrancorosa@gmail.com>
2020-02-28 18:04:07 -0300

    Add Glide

---
+++ ChallengeAlpha/app/src/main/java/com/hurb/challengealpha/MyAppGlideModule.java
@@ -0,0 +1,8 @@
+package com.hurb.challengealpha;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+}
