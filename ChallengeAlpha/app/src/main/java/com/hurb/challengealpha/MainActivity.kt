0af1d2dccfd493a8e8db2dd033514d5354eae6b3
Alexsander Franco <alexsanderfrancorosa@gmail.com>
2020-02-28 18:06:09 -0300

    Add ViewBinding

--- ChallengeAlpha/app/src/main/java/com/hurb/challengealpha/MainActivity.kt
+++ ChallengeAlpha/app/src/main/java/com/hurb/challengealpha/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.hurb.challengealpha
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hurb.challengealpha.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
