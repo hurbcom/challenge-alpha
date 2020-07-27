86f9e57e7cd030cbed512e4dec6e147dedc67aa8
Helio Junior <helio.engenharia@gmail.com>
2020-07-26 23:20:21 -0300

    remove comments.

--- HurbHotel_iOS/HurbHotel_iOS/Commons/Model/Filters.swift
+++ HurbHotel_iOS/HurbHotel_iOS/Commons/Model/Filters.swift
@@ -12,21 +12,6 @@ class Filters: Decodable {
     let amenities: [Filter]?
     let prices: [Price]?
     
-    //Poderia fazer os sub-objetos de Filters, para o teste não há necessidade.
-//    let attributes: [Filter]?
-//    let cities: [Filter]?
-//    let countries: [Filter]?
-//    let departureCities: [Filter]?
-//    let duration: [Filter]?
-//    let food: [Filter]?
-//    let people: [Filter]?
-//    let priceInterval: [Filter]?
-//    let productType: [Filter]?
-//    let regulation: [Filter]?
-//    let rooms: [Filter]?
-//    let stars: [Filter]?
-//    let states: [Filter]?
-    
     struct Filter: Decodable {
         let count: Int
         let filter: String
