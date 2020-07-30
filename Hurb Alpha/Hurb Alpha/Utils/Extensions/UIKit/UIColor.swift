b3cc64ace1513367b2654437f64786459972e307
sev <thiago@guarana.io>
2020-07-30 19:45:58 -0300

    .gitignore is now working

---
+++ Hurb Alpha/Hurb Alpha/Utils/Extensions/UIKit/UIColor.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor.swift
+//  Hurb Alpha
+//
+//  Created by Thiago Augusto on 23/07/20.
+//  Copyright © 2020 objectivesev. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    /// #143a7c
+    static let primaryColor = UIColor(red: 0.08, green: 0.23, blue: 0.49, alpha: 1.00)
+    
+    /// #FF6D00
+    static let secondaryColor = UIColor(red: 1.00, green: 0.43, blue: 0.00, alpha: 1.00)
+}
