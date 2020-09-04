53187bbc2b8a92fcdd3c7d707e1d65a68791af3f
Edson Guido <edson.guido@uds.com.br>
2020-09-04 15:38:34 -0300

    Adicionado git ignore

--- Pods/Target Support Files/Pods-DesafioHurb-DesafioHurbUITests/Pods-DesafioHurb-DesafioHurbUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DesafioHurb_DesafioHurbUITests : NSObject
-@end
-@implementation PodsDummy_Pods_DesafioHurb_DesafioHurbUITests
-@end
--- Pods/Target Support Files/Pods-DesafioHurbTests/Pods-DesafioHurbTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DesafioHurbTests : NSObject
-@end
-@implementation PodsDummy_Pods_DesafioHurbTests
-@end
