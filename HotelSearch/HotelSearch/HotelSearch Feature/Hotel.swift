caf8c03fb30d0dec78f4abdf0d6833ca58f5b57c
Tulio Parreiras <tulio@usemobile.xyz>
2020-09-13 17:57:05 -0300

    Make Hotel public

--- HotelSearch/HotelSearch/HotelSearch Feature/Hotel.swift
+++ HotelSearch/HotelSearch/HotelSearch Feature/Hotel.swift
@@ -8,43 +8,84 @@
 
 import Foundation
 
-struct Hotel {
-    let amenities: [Amenity]
-    let category: String
-    let description: String
-    let gallery: [HotelImage]
-    let id: Int
-    let image: URL
-    let isHotel: Bool
-    let name: String
-    let price: HotelPrice
-    let quantityDescriptors: QuantityDescriptor
-    let smallDescription: String
-    let star: Int
-    let tags: [String]
-    let url: URL
+public struct Hotel {
+    public let amenities: [Amenity]
+    public let category: String
+    public let description: String
+    public let gallery: [HotelImage]
+    public let id: Int
+    public let image: URL
+    public let isHotel: Bool
+    public let name: String
+    public let price: HotelPrice
+    public let quantityDescriptors: QuantityDescriptor
+    public let smallDescription: String
+    public let star: Int
+    public let tags: [String]
+    public let url: URL
+    
+    public init(amenities: [Amenity], category: String, description: String, gallery: [HotelImage], id: Int, image: URL, isHotel: Bool, name: String, price: HotelPrice, quantityDescriptors: QuantityDescriptor, smallDescription: String, star: Int, tags: [String], url: URL) {
+        self.amenities = amenities
+        self.category = category
+        self.description = description
+        self.gallery = gallery
+        self.id = id
+        self.image = image
+        self.isHotel = isHotel
+        self.name = name
+        self.price = price
+        self.quantityDescriptors = quantityDescriptors
+        self.smallDescription = smallDescription
+        self.star = star
+        self.tags = tags
+        self.url = url
+    }
 }
 
-struct Amenity {
-    let count: Int
-    let filter: String
-    let term: String
+public struct Amenity {
+    public let count: Int
+    public let filter: String
+    public let term: String
+    
+    public init(count: Int, filter: String, term: String) {
+        self.count = count
+        self.filter = filter
+        self.term = term
+    }
 }
 
-struct HotelImage {
-    let description: String
-    let roomId: String?
-    let url: URL
+public struct HotelImage {
+    public let description: String
+    public let roomId: String?
+    public let url: URL
+    
+    public init(description: String, roomId: String?, url: URL) {
+        self.description = description
+        self.roomId = roomId
+        self.url = url
+    }
 }
 
-struct HotelPrice {
-    let amount: Double
-    let amountPerDay: Double
-    let currency: String
+public struct HotelPrice {
+    public let amount: Double
+    public let amountPerDay: Double
+    public let currency: String
+    
+    public init(amount: Double, amountPerDay: Double, currency: String) {
+        self.amount = amount
+        self.amountPerDay = amountPerDay
+        self.currency = currency
+    }
 }
 
-struct QuantityDescriptor {
-    let maxAdults: Int
-    let maxChildren: Int
-    let maxFreeChildrenAge: Int
+public struct QuantityDescriptor {
+    public let maxAdults: Int
+    public let maxChildren: Int
+    public let maxFreeChildrenAge: Int
+    
+    public init(maxAdults: Int, maxChildren: Int, maxFreeChildrenAge: Int) {
+        self.maxAdults = maxAdults
+        self.maxChildren = maxChildren
+        self.maxFreeChildrenAge = maxFreeChildrenAge
+    }
 }
