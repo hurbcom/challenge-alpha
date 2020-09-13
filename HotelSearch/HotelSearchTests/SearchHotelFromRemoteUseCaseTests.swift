2889d3596b24db617c3d650148beb06732574193
Tulio Parreiras <tulio@usemobile.xyz>
2020-09-13 18:09:38 -0300

    Search hotel does delivers error on non 200 response

--- HotelSearch/HotelSearchTests/SearchHotelFromRemoteUseCaseTests.swift
+++ HotelSearch/HotelSearchTests/SearchHotelFromRemoteUseCaseTests.swift
@@ -8,27 +8,50 @@
 
 import XCTest
 
+import HotelSearch
+
 final class RemoteHotelSearcher {
     
     private let url: URL
     private let client: HTTPClientSpy
+    
+    public enum Error: Swift.Error {
+        case invalidData
+    }
     
     init(url: URL, client: HTTPClientSpy) {
         self.url = url
         self.client = client
     }
     
-    func searchHotel(with searchText: String) {
-        self.client.get(from: url)
+    func searchHotel(with searchText: String, competion: @escaping (Result<[Hotel], Error>) -> Void) {
+        self.client.get(from: url) { result in
+            competion(.failure(.invalidData))
+        }
     }
     
 }
 
 final class HTTPClientSpy {
-    var requestedURLs = [URL]()
+    private var messages = [(url: URL, completion: (Result<(Data, HTTPURLResponse), Error>) -> Void)]()
     
-    func get(from url: URL) {
-        self.requestedURLs.append(url)
+    var requestedURLs: [URL] {
+        return messages.compactMap { $0.url }
+    }
+    
+    func get(from url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
+        messages.append((url, completion))
+    }
+    
+    
+    func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
+        let response = HTTPURLResponse(
+            url: requestedURLs[index],
+            statusCode: code,
+            httpVersion: nil,
+            headerFields: nil
+        )!
+        messages[index].completion(.success((data, response)))
     }
 }
 
@@ -44,7 +67,7 @@ class SearchHotelFromRemoteUseCaseTests: XCTestCase {
         let url = URL(string: "https://any-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.searchHotel(with: "")
+        sut.searchHotel(with: "") { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url])
     }
@@ -53,10 +76,30 @@ class SearchHotelFromRemoteUseCaseTests: XCTestCase {
         let url = URL(string: "https://any-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.searchHotel(with: "")
-        sut.searchHotel(with: "")
+        sut.searchHotel(with: "") { _ in }
+        sut.searchHotel(with: "") { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
+    func test_searchHotel_deliversErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        
+        let exp = expectation(description: "Wait for search completion")
+        
+        sut.searchHotel(with: "") { receivedResult in
+            switch receivedResult {
+            case .success:
+                XCTFail("Expected to receive error, got \(receivedResult) instead")
+            case .failure: break
+            }
+            exp.fulfill()
+        }
+        
+        let emptyListJson = makeHotelsJSON([])
+        client.complete(withStatusCode: 199, data: emptyListJson)
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     // MARK: Helpers
@@ -65,6 +108,11 @@ class SearchHotelFromRemoteUseCaseTests: XCTestCase {
         let client = HTTPClientSpy()
         let sut = RemoteHotelSearcher(url: url, client: client)
         return (sut, client)
+    }
+    
+    private func makeHotelsJSON(_ items: [[String: Any]]) -> Data {
+        let json = ["result": items]
+        return try! JSONSerialization.data(withJSONObject: json)
     }
 
 }
