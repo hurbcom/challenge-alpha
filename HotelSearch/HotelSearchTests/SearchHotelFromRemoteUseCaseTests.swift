9b882381d20f4f161b6ef3f0003d0b30bb85c807
Tulio Parreiras <tulio@usemobile.xyz>
2020-09-13 13:22:18 -0300

    Hotel search does request data from url

--- HotelSearch/HotelSearchTests/SearchHotelFromRemoteUseCaseTests.swift
+++ HotelSearch/HotelSearchTests/SearchHotelFromRemoteUseCaseTests.swift
@@ -10,19 +10,45 @@ import XCTest
 
 final class RemoteHotelSearcher {
     
+    private let url: URL
+    private let client: HTTPClientSpy
+    
+    init(url: URL, client: HTTPClientSpy) {
+        self.url = url
+        self.client = client
+    }
+    
+    func searchHotel(with searchText: String) {
+        self.client.get(from: url)
+    }
+    
 }
 
 final class HTTPClientSpy {
     var requestedURLs = [URL]()
+    
+    func get(from url: URL) {
+        self.requestedURLs.append(url)
+    }
 }
 
 class SearchHotelFromRemoteUseCaseTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        let _ = RemoteHotelSearcher()
+        let _ = RemoteHotelSearcher(url: URL(string: "https://any-url.com")!, client: client)
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    func test_searchHotel_requestsDataFromURL() {
+        let url = URL(string: "https://any-url.com")!
+        let client = HTTPClientSpy()
+        let sut = RemoteHotelSearcher(url: url, client: client)
+        
+        sut.searchHotel(with: "")
+        
+        XCTAssertEqual(client.requestedURLs, [url])
     }
 
 }
