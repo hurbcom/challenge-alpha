535cc9d4bfd428ca6a970368ef57eacec6788f54
Tulio Parreiras <tulio@usemobile.xyz>
2020-09-14 08:28:21 -0300

    Make HotelSearchViewController root from application window

--- HotelSearch/HotelSearchiOS/ViewController/HotelSearchViewController.swift
+++ HotelSearch/HotelSearchiOS/ViewController/HotelSearchViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 final public class HotelSearchViewController: UIViewController {
-
+    
+    public init() {
+        super.init(nibName: nil, bundle: Bundle(for: HotelSearchViewController.self))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
     }
