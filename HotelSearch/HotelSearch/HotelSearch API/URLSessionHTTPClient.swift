0d1b6e653cbaf7d08182ce99c8c63194056487e2
Tulio Parreiras <tulio@usemobile.xyz>
2020-09-14 18:40:43 -0300

    Create URLSessionHTTPClient

---
+++ HotelSearch/HotelSearch/HotelSearch API/URLSessionHTTPClient.swift
@@ -0,0 +1,34 @@
+//
+//  URLSessionHTTPClient.swift
+//  HotelSearch
+//
+//  Created by Tulio Parreiras on 14/09/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import Foundation
+
+public final class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValuesRepresentation: Error {}
+    
+    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+        let task = session.dataTask(with: url) { data, response, error in
+            completion(Result {
+                if let error = error {
+                    throw error
+                } else if let data = data, let response = response as? HTTPURLResponse {
+                    return (data, response)
+                } else {
+                    throw UnexpectedValuesRepresentation()
+                }
+            })
+        }
+        task.resume()
+    }
+}
