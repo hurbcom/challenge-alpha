bde10b198b0d372795c2f87c29dd599c0676e46a
Tulio Parreiras <tulio@usemobile.xyz>
2020-09-14 18:48:13 -0300

    Create HotelSearchViewModel

--- HotelSearch/HotelSearchiOS/ViewController/HotelSearchViewController.swift
+++ HotelSearch/HotelSearchiOS/ViewController/HotelSearchViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
+import HotelSearch
+
+final public class HotelSearchViewModel {
+    
+    var text: String = "buzios"
+    
+    private let hotelSearcher: HotelSearcher
+    
+    public init(hotelSearcher: HotelSearcher) {
+        self.hotelSearcher = hotelSearcher
+    }
+    
+    func searchHotel() {
+        let searchText = self.text + "&page=1"
+        self.hotelSearcher.searchHotel(with: searchText) { [weak self] result in
+            switch result {
+            case let .success(hotels):
+                print(hotels)
+            case let .failure(error):
+                print(error)
+            }
+        }
+    }
+    
+}
+
 final public class HotelSearchViewController: UIViewController {
     
     // MARK: - IBOutlets
     
-    @IBOutlet weak var inputView: UIView!
+    @IBOutlet weak var viewInput: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var btnSearch: UIButton!
     @IBOutlet weak var collectionView: UICollectionView!
@@ -20,12 +46,17 @@ final public class HotelSearchViewController: UIViewController {
     // MARK: - IBActions
     
     @IBAction func btnSearchAction(_ sender: UIButton) {
-        
+        self.viewModel.searchHotel()
     }
+    
+    // MARK: - Properties
+    
+    private let viewModel: HotelSearchViewModel
     
     // MARK: - Life Cycle
     
-    public init() {
+    public init(viewModel: HotelSearchViewModel) {
+        self.viewModel = viewModel
         super.init(nibName: nil, bundle: Bundle(for: HotelSearchViewController.self))
     }
     
--- hurb-test/hurb-test/SceneDelegate.swift
+++ hurb-test/hurb-test/SceneDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+import HotelSearch
 import HotelSearchiOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -21,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let controller = HotelSearchViewController()
+        let controller = HotelSearchViewController(viewModel: HotelSearchViewModel(hotelSearcher: RemoteHotelSearcher(url: URL(string: "https://www.hurb.com/search/api?q=")!, client: URLSessionHTTPClient(session: .shared))))
         window.rootViewController = controller
         window.makeKeyAndVisible()
         self.window = window
