c3ecad942078f0999d078dd48117d0a3479d3149
Tulio Parreiras <tulio@usemobile.xyz>
2020-09-14 07:28:32 -0300

    Hotel search does deliver items on 200 response with json items

--- HotelSearch/HotelSearchTests/SearchHotelFromRemoteUseCaseTests.swift
+++ HotelSearch/HotelSearchTests/SearchHotelFromRemoteUseCaseTests.swift
@@ -190,7 +190,7 @@ class SearchHotelFromRemoteUseCaseTests: XCTestCase {
         let samples = [199, 201, 300, 400, 500]
         samples.enumerated().forEach { index, code in
             expect(sut, toCompleteWith: .failure(RemoteHotelSearcher.Error.invalidData), when: {
-                let emptyListJson = makeHotelsJSON([])
+                let emptyListJson = makeItemsJSON([])
                 client.complete(withStatusCode: code, data: emptyListJson, at: index)
             })
         }
@@ -209,9 +209,25 @@ class SearchHotelFromRemoteUseCaseTests: XCTestCase {
         let (sut, client) = makeSUT()
         
         expect(sut, toCompleteWith: .success([]), when: {
-            let emptyListJson = makeHotelsJSON([])
+            let emptyListJson = makeItemsJSON([])
             client.complete(withStatusCode: 200, data: emptyListJson)
         })
+    }
+    
+    func test_searchHotel_deliversItemsOn200HTTPResponseWithJSONItems() {
+        let (sut, client) = makeSUT()
+        
+        let item1 = makeItem(category: "a category", description: "a description", id: 1, image: URL(string: "https://a-image-url.com")!, isHotel: true, name: "a name", smallDescription: "a small description", star: 1, url: URL(string: "https://a-url.com")!)
+        
+        let item2 = makeItem(category: "another category", description: "another description", id: 2, image: URL(string: "https://another-image-url.com")!, isHotel: false, name: "another name", smallDescription: "another small description", star: 2, url: URL(string: "https://another-url.com")!)
+        
+        let items = [item1.model, item2.model]
+        
+        expect(sut, toCompleteWith: .success(items), when: {
+            let json = makeItemsJSON([item1.json, item2.json])
+            client.complete(withStatusCode: 200, data: json)
+        })
+        
     }
     
     // MARK: Helpers
@@ -220,11 +236,6 @@ class SearchHotelFromRemoteUseCaseTests: XCTestCase {
         let client = HTTPClientSpy()
         let sut = RemoteHotelSearcher(url: url, client: client)
         return (sut, client)
-    }
-    
-    private func makeHotelsJSON(_ items: [[String: Any]]) -> Data {
-        let json = ["result": items]
-        return try! JSONSerialization.data(withJSONObject: json)
     }
     
     private func expect(_ sut: RemoteHotelSearcher, toCompleteWith expectedResult: RemoteHotelSearcher.Result, when action: () -> Void) {
@@ -249,5 +260,75 @@ class SearchHotelFromRemoteUseCaseTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
     }
+    
+    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+        let json = ["result": items]
+        return try! JSONSerialization.data(withJSONObject: json)
+    }
+    
+    private func makeItem(
+        amenities: [Amenity] = [],
+        category: String,
+        description: String,
+        gallery: [HotelImage] = [],
+        id: Int,
+        image: URL,
+        isHotel: Bool,
+        name: String,
+        price: HotelPrice = HotelPrice(amount: 12.34, amountPerDay: 43.21, currency: "a currency"),
+        quantityDescriptors: QuantityDescriptor = QuantityDescriptor(maxAdults: 5, maxChildren: 6, maxFreeChildrenAge: 7),
+        smallDescription: String,
+        star: Int,
+        tags: [String] = [],
+        url: URL) -> (model: Hotel, json: [String: Any]) {
+        let item = Hotel(amenities: amenities, category: category, description: description, gallery: gallery, id: id, image: image, isHotel: isHotel, name: name, price: price, quantityDescriptors: quantityDescriptors, smallDescription: smallDescription, star: star, tags: tags, url: url)
+        
+        return (item, item.json)
+    }
 
+}
+
+private extension Hotel {
+    var json: [String: Any] {
+        return [
+            "amenities": amenities.map{ $0.json },
+            "category": category,
+            "description": description,
+            "gallery": gallery.map{ $0.json },
+            "id": id,
+            "image": image.absoluteString,
+            "isHotel": isHotel,
+            "name": name,
+            "price": price.json,
+            "quantityDescriptors": quantityDescriptors.json,
+            "smallDescription": smallDescription,
+            "star": star,
+            "tags": tags,
+            "url": url.absoluteString
+        ]
+    }
+}
+
+private extension Amenity {
+    var json: [String: Any] {
+        return ["category": category,"name": name]
+    }
+}
+
+private extension HotelImage {
+    var json: [String: Any] {
+        return ["description": description, "url": url.absoluteString]
+    }
+}
+
+private extension HotelPrice {
+    var json: [String: Any] {
+        return ["amount": amount, "amountPerDay": amountPerDay, "currency": currency]
+    }
+}
+
+private extension QuantityDescriptor {
+    var json: [String: Any] {
+        return ["maxAdults": maxAdults, "maxChildren": maxChildren, "maxFreeChildrenAge": maxFreeChildrenAge]
+    }
 }
