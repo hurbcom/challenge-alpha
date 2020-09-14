cb40cadd7d050a92241e3740159c2d8a45974fea
Tulio Parreiras <tulio@usemobile.xyz>
2020-09-14 08:23:58 -0300

    Create HotelSearchViewController

---
+++ HotelSearch/HotelSearchiOS/ViewController/HotelSearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HotelSearchViewController.swift
+//  HotelSearchiOS
+//
+//  Created by Tulio Parreiras on 14/09/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import UIKit
+
+class HotelSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
