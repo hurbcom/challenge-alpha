3d30a46798068660d07b26450964fa35ee5dac88
Tulio Parreiras <tulio@usemobile.xyz>
2020-09-19 13:53:11 -0300

    Adjust HTTPClientSpy to conform to HTTPClient

--- HotelSearch/HotelSearchTests/HotelSearch API/Helpers/HTTPClientSpy.swift
+++ HotelSearch/HotelSearchTests/HotelSearch API/Helpers/HTTPClientSpy.swift
@@ -11,14 +11,23 @@ import Foundation
 import HotelSearch
 
 final class HTTPClientSpy: HTTPClient {
+    private struct Task: HTTPClientTask {
+        let callback: () -> Void
+        func cancel() { callback() }
+    }
+    
     private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+    private(set) var cancelledURLs = [URL]()
     
     var requestedURLs: [URL] {
         return messages.compactMap { $0.url }
     }
     
-    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
         messages.append((url, completion))
+        return Task { [weak self] in
+            self?.cancelledURLs.append(url)
+        }
     }
     
     func complete(with error: Error, at index: Int = 0) {
