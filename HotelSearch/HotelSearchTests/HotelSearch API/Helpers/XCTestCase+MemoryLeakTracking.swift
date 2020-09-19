e634c5dd2d4914b698ca8bc36bcf84be75a2dc80
Tulio Parreiras <tulio@usemobile.xyz>
2020-09-19 20:07:01 -0300

    Move memory leaking track to helper file

---
+++ HotelSearch/HotelSearchTests/HotelSearch API/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,19 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift
+//  HotelSearchTests
+//
+//  Created by Tulio Parreiras on 19/09/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import XCTest
+
+extension XCTestCase {
+    
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+    
+}
--- HotelSearch/HotelSearchTests/HotelSearch API/SearchHotelFromRemoteUseCaseTests.swift
+++ HotelSearch/HotelSearchTests/HotelSearch API/SearchHotelFromRemoteUseCaseTests.swift
@@ -130,12 +130,6 @@ class SearchHotelFromRemoteUseCaseTests: XCTestCase {
         return .failure(error)
     }
     
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
-        }
-    }
-    
     private func expect(_ sut: RemoteHotelSearcher, toCompleteWith expectedSearchResult: RemoteHotelSearcher.SearchResult, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         
         let exp = expectation(description: "Wait for search completion")
