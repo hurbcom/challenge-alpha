ca72fd656e2071d7751d16003063397f322e7695
Tulio Parreiras <tulio@usemobile.xyz>
2020-09-20 10:59:59 -0300

    Does render loaded hotels successfully after hotel search completion

--- hurb-test/hurb-testTests/HotelSearchUIIntegrationTests.swift
+++ hurb-test/hurb-testTests/HotelSearchUIIntegrationTests.swift
@@ -24,7 +24,7 @@ class HotelSearchUIIntegrationTests: XCTestCase {
         XCTAssertEqual(spy.searchHotelCallCount, 1)
     }
     
-    func test_loadingHotelIndicator_isVisibleWhileSearchingHotel() {
+    func test_loadingHotelIndicator_isVisibleWhileSearchingHotels() {
         let spy = Spy()
         let sut = HotelSearchUIComposer.hotelSearchComposedWith(hotelSearcher: spy, imageDataLoader: spy)
         sut.loadViewIfNeeded()
@@ -37,7 +37,74 @@ class HotelSearchUIIntegrationTests: XCTestCase {
         XCTAssertFalse(sut.isShowingLoadingIndicator)
     }
     
+    func test_searchHotelCompletion_rendersSuccessfullyLoadedHotels() {
+        let hotel0 = makeItem(name: "a name", stars: 4)
+        let hotel1 = makeItem(name: "another name", stars: 4)
+        let hotel2 = makeItem(address: Address(city: "city", country: nil, state: "state", street: nil, zipcode: nil), name: "a new name", stars: 4)
+        let hotel3 = makeItem(amenities: [Amenity(category: nil, name: "amenity"), Amenity(category: nil, name: "a amenity")], stars: 3)
+        let spy = Spy()
+        let sut = HotelSearchUIComposer.hotelSearchComposedWith(hotelSearcher: spy, imageDataLoader: spy)
+        
+        sut.loadViewIfNeeded()
+        assertThat(sut, isRendering: [])
+        
+        sut.simulateHotelSearch()
+        spy.completeHotelSearch(with: [hotel0])
+        assertThat(sut, isRendering: [[hotel0]])
+        
+        sut.simulateHotelSearch()
+        spy.completeHotelSearch(with: [hotel0, hotel1, hotel2, hotel3], at: 1)
+        assertThat(sut, isRendering: [[hotel0, hotel1, hotel2], [hotel3]])
+    }
+    
     // MARK: - Helpers
+    
+    private func makeItem(
+        address: Address = Address(city: "a city", country: "a country", state: "a state", street: "a street", zipcode: "a zipcode"),
+        amenities: [Amenity] = [Amenity(category: "a category", name: "a name")],
+        category: String? = nil,
+        description: String? = nil,
+        gallery: [HotelImage] = [HotelImage(description: "a description", url: URL(string: "https://a-url.com")!)],
+        id: String? = nil,
+        image: URL? = nil,
+        isHotel: Bool? = nil,
+        name: String? = nil,
+        price: HotelPrice = HotelPrice(amount: 12.34, amountPerDay: 43.21, currency: "a currency"),
+        quantityDescriptors: QuantityDescriptor = QuantityDescriptor(maxAdults: 5, maxChildren: 6, maxFreeChildrenAge: 7),
+        smallDescription: String? = nil,
+        stars: Int,
+        tags: [String] = [],
+        url: URL? = nil) -> Hotel {
+        return Hotel(address: address, amenities: amenities, category: category, description: description, gallery: gallery, id: id, image: image, isHotel: isHotel, name: name, price: price, quantityDescriptors: quantityDescriptors, smallDescription: smallDescription, stars: stars, tags: tags, url: url)
+    }
+
+    func assertThat(_ sut: HotelSearchViewController, isRendering hotels: [[Hotel]], file: StaticString = #file, line: UInt = #line) {
+        sut.view.enforceLayoutCycle()
+        
+        hotels.enumerated().forEach { section, hotelArr in
+            XCTAssertEqual(hotelArr.count, sut.numberOfRenderedHotelCells(inSction: section))
+            hotelArr.enumerated().forEach { index, hotel in
+                assertThat(sut, hasViewConfiguredFor: hotel, at: index, section: section)
+            }
+        }
+    }
+    
+    func assertThat(_ sut: HotelSearchViewController, hasViewConfiguredFor hotel: Hotel, at index: Int, section: Int, file: StaticString = #file, line: UInt = #line) {
+        let view = sut.hotelCell(at: index, section: section)
+        
+        guard let cell = view as? HotelCell else {
+            return XCTFail("Expected \(HotelCell.self) instance, got \(String(describing: view)) instead", file: file, line: line)
+        }
+        
+        let shouldNameBeVisible = hotel.name != nil
+        let shouldLocationBeVisible = hotel.address?.city != nil || hotel.address?.state != nil
+        let shouldAmenitiesBeVisible = (hotel.amenities?.count ?? 0) > 0
+        let shouldPriceBeVisible = hotel.price?.amountPerDay != nil
+        XCTAssertEqual(cell.isShowingName, shouldNameBeVisible, file: file, line: line)
+        XCTAssertEqual(cell.isShowingLocation, shouldLocationBeVisible, file: file, line: line)
+        XCTAssertEqual(cell.isShowingAmenities, shouldAmenitiesBeVisible, file: file, line: line)
+        XCTAssertEqual(cell.isShowingPrice, shouldPriceBeVisible, file: file, line: line)
+    }
     
     private class Spy: HotelSearcher, ImageDataLoader {
         
@@ -80,4 +147,41 @@ extension HotelSearchViewController {
         self.btnSearch.sendActions(for: .touchUpInside)
     }
     
+    func numberOfRenderedHotelCells(inSction section: Int = 0) -> Int {
+        return tableView.numberOfRows(inSection: section)
+    }
+    
+    func hotelCell(at row: Int, section: Int) -> UITableViewCell? {
+        guard numberOfRenderedHotelCells(inSction: section) > row else {
+            return nil
+        }
+        let ds = tableView.dataSource
+        let index = IndexPath(row: row, section: section)
+        return ds?.tableView(tableView, cellForRowAt: index)
+    }
+    
+}
+
+extension HotelCell {
+    
+    var isShowingName: Bool {
+        return self.lblName.text != nil
+    }
+    var isShowingLocation: Bool {
+        return self.lblLocation.text != nil
+    }
+    var isShowingAmenities: Bool {
+        return self.lblAmenities.text != nil
+    }
+    var isShowingPrice: Bool {
+        return self.lblPrice.text != nil
+    }
+    
+}
+
+extension UIView {
+    func enforceLayoutCycle() {
+        layoutIfNeeded()
+        RunLoop.current.run(until: Date())
+    }
 }
