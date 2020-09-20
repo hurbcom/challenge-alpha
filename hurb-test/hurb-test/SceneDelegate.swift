b73a47b58ea6df4ba5b381eace27e7923e9af022
Tulio Parreiras <tulio@usemobile.xyz>
2020-09-20 12:33:08 -0300

    Does set window as key and visible

--- hurb-test/hurb-test/SceneDelegate.swift
+++ hurb-test/hurb-test/SceneDelegate.swift
@@ -15,21 +15,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: windowScene)
+        
+        self.window = UIWindow(windowScene: windowScene)
+        self.configureWindow()
+    }
+
+    func configureWindow() {
         let hotelSearcher = RemoteHotelSearcher(url: URL(string: "https://www.hurb.com/search/api?q=")!, client: URLSessionHTTPClient(session: .shared))
         let imageDataLoader = RemoteImageDataLoader(client: URLSessionHTTPClient(session: .shared))
         let controller = HotelSearchUIComposer.hotelSearchComposedWith(hotelSearcher: hotelSearcher, imageDataLoader: imageDataLoader)
-        window.rootViewController = controller
-        window.makeKeyAndVisible()
-        self.window = window
+        window?.rootViewController = controller
+        window?.makeKeyAndVisible()
     }
-
 
 }
 
---
+++ hurb-test/hurb-testTests/SceneDelegateTests.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegateTests.swift
+//  hurb-testTests
+//
+//  Created by Tulio Parreiras on 20/09/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import XCTest
+@testable import hurb_test
+
+class SceneDelegateTests: XCTestCase {
+    
+    func test_configureWindow_setsWindowAsKeyAndVisible() {
+        let window = UIWindow()
+        let sut = SceneDelegate()
+        sut.window = window
+        
+        sut.configureWindow()
+        
+        XCTAssertTrue(window.isKeyWindow, "Expected window to be key window")
+        XCTAssertFalse(window.isHidden, "Expected window to be visible")
+    }
+
+}
