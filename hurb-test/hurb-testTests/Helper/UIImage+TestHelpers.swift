31851102d405805fb7ecd8f08969a2a7dfae87e7
Tulio Parreiras <tulio@usemobile.xyz>
2020-09-20 12:37:56 -0300

    Move UIImage test helpers to a separated file

---
+++ hurb-test/hurb-testTests/Helper/UIImage+TestHelpers.swift
@@ -0,0 +1,22 @@
+//
+//  UIImage+TestHelpers.swift
+//  hurb-testTests
+//
+//  Created by Tulio Parreiras on 20/09/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    static func make(withColor color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img!
+    }
+}
--- hurb-test/hurb-testTests/HotelSearchUIIntegrationTests.swift
+++ hurb-test/hurb-testTests/HotelSearchUIIntegrationTests.swift
@@ -503,17 +503,3 @@ extension UIView {
         RunLoop.current.run(until: Date())
     }
 }
-
-
-extension UIImage {
-    static func make(withColor color: UIColor) -> UIImage {
-        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-        UIGraphicsBeginImageContext(rect.size)
-        let context = UIGraphicsGetCurrentContext()!
-        context.setFillColor(color.cgColor)
-        context.fill(rect)
-        let img = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return img!
-    }
-}
