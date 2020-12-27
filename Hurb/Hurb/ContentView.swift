c76b17b4d3f4528df114980c3d441c106bf245a0
Arthur Givigir <arthur316@gmail.com>
2020-12-27 11:12:53 -0300

    [ADD] First commit

---
+++ Hurb/Hurb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hurb
+//
+//  Created by Arthur Givigir on 12/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hurb/Hurb/HurbApp.swift
@@ -0,0 +1,17 @@
+//
+//  HurbApp.swift
+//  Hurb
+//
+//  Created by Arthur Givigir on 12/27/20.
+//
+
+import SwiftUI
+
+@main
+struct HurbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
