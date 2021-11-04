81aaddc09a385be6eb552baf3477a4d34b85765e
Theo Mendes <theoafmendes@gmail.com>
2021-11-04 16:50:09 -0300

    🚧 [MOB-2586] - Created SwiftUI Views

--- Projects/Challenge Alpha iOS/Shared/ContentView.swift
+++ Projects/Challenge Alpha iOS/Shared/ContentView.swift
@@ -6,11 +6,71 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @State private var selection: String? = nil
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack(alignment: .center) {
+                NavigationLink(destination: ResultRenderView(type: .search)) {
+                    Text("Search")
+                        .fontWeight(.bold)
+                        .font(.title)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .padding(10)
+                }.buttonStyle(PlainButtonStyle())
+                
+                NavigationLink(destination: ResultRenderView(type: .searchPackage)) {
+                    Button(action: {
+                        debugPrint("Search Package")
+                    }) {
+                        Text("Search Package")
+                            .fontWeight(.bold)
+                            .font(.title)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .padding(10)
+                    }
+                }
+                
+                NavigationLink(destination: ResultRenderView(type: .searchHotel)) {
+                    Button(action: {
+                        debugPrint("Search Hotel")
+                    }) {
+                        Text("Search Hotel")
+                            .fontWeight(.bold)
+                            .font(.title)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .padding(10)
+                    }
+                }
+                
+                NavigationLink(destination: ResultRenderView(type: .suggestion)) {
+                    Button(action: {
+                        debugPrint("Suggestion")
+                    }) {
+                        Text("Suggestion")
+                            .fontWeight(.bold)
+                            .font(.title)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(40)
+                            .foregroundColor(.white)
+                            .padding(10)
+                    }
+                }
+            }
+        }
     }
 }
 
