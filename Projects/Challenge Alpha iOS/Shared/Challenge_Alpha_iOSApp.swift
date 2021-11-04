ad9b6e2303fdb7bac0ad516ff0e072b0ea3d6664
Theo Mendes <theoafmendes@gmail.com>
2021-11-04 10:36:59 -0300

    🎉 [MOB-2586] - Started exemple project iOS

---
+++ Projects/Challenge Alpha iOS/Shared/Challenge_Alpha_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_Alpha_iOSApp.swift
+//  Shared
+//
+//  Created by Theo Mendes on 04/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_Alpha_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
