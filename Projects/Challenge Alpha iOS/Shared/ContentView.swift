ba528f8dda6655b1b1d15b7cedb2f527d400cef1
Theo Mendes <theoafmendes@gmail.com>
2021-11-08 14:18:11 -0300

    🩹 [MOB-2586] - Fixed buttons

--- Projects/Challenge Alpha iOS/Shared/ContentView.swift
+++ Projects/Challenge Alpha iOS/Shared/ContentView.swift
@@ -26,49 +26,37 @@ struct ContentView: View {
                 }.buttonStyle(PlainButtonStyle())
                 
                 NavigationLink(destination: ResultRenderView(type: .searchPackage)) {
-                    Button(action: {
-                        debugPrint("Search Package")
-                    }) {
-                        Text("Search Package")
-                            .fontWeight(.bold)
-                            .font(.title)
-                            .padding()
-                            .background(Color.blue)
-                            .cornerRadius(40)
-                            .foregroundColor(.white)
-                            .padding(10)
-                    }
-                }
+                    Text("Search Package")
+                        .fontWeight(.bold)
+                        .font(.title)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .padding(10)
+                }.buttonStyle(PlainButtonStyle())
                 
                 NavigationLink(destination: ResultRenderView(type: .searchHotel)) {
-                    Button(action: {
-                        debugPrint("Search Hotel")
-                    }) {
-                        Text("Search Hotel")
-                            .fontWeight(.bold)
-                            .font(.title)
-                            .padding()
-                            .background(Color.blue)
-                            .cornerRadius(40)
-                            .foregroundColor(.white)
-                            .padding(10)
-                    }
-                }
+                    Text("Search Hotel")
+                        .fontWeight(.bold)
+                        .font(.title)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .padding(10)
+                }.buttonStyle(PlainButtonStyle())
                 
                 NavigationLink(destination: ResultRenderView(type: .suggestion)) {
-                    Button(action: {
-                        debugPrint("Suggestion")
-                    }) {
-                        Text("Suggestion")
-                            .fontWeight(.bold)
-                            .font(.title)
-                            .padding()
-                            .background(Color.blue)
-                            .cornerRadius(40)
-                            .foregroundColor(.white)
-                            .padding(10)
-                    }
-                }
+                    Text("Suggestion")
+                        .fontWeight(.bold)
+                        .font(.title)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .padding(10)
+                }.buttonStyle(PlainButtonStyle())
             }
         }
     }
