3ab597531c75c4bdee3c56faee072ba484759c97
Israel <israel.nascimento@b2wdigital.com>
2022-02-12 15:07:48 -0300

    feat: show bottom navigation view

--- android/hotels-list/src/main/java/com/isranascimento/hotelslist/ui/viewmodels/fragment/HotelListFragment.kt
+++ android/hotels-list/src/main/java/com/isranascimento/hotelslist/ui/viewmodels/fragment/HotelListFragment.kt
@@ -1,26 +1,27 @@
-package com.isranascimento.challengealpha.ui
+package com.isranascimento.hotelslist.ui.viewmodels.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.isranascimento.challengealpha.R
-import com.isranascimento.challengealpha.databinding.HomeFragmentBinding
+import com.isranascimento.hotelslist.databinding.HotelListFragmentBinding
 
-class HomeFragment: Fragment() {
-    private lateinit var binding: HomeFragmentBinding
+class HotelListFragment: Fragment() {
+    private lateinit var binding: HotelListFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = HomeFragmentBinding.inflate(inflater, container, false)
+    ): View {
+        return HotelListFragmentBinding.inflate(inflater, container, false)
             .also {
                 binding = it
             }.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        activity?.title = getString(R.string.app_name)
+        super.onViewCreated(view, savedInstanceState)
     }
 }
