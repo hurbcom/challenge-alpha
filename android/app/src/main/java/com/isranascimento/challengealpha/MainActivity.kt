4a3d754cd3b8ef530a362dde9d65ddecfc6e25f3
Israel <israel.nascimento@b2wdigital.com>
2022-02-12 13:30:08 -0300

    feat: HomeFragment

--- android/app/src/main/java/com/isranascimento/challengealpha/MainActivity.kt
+++ android/app/src/main/java/com/isranascimento/challengealpha/MainActivity.kt
@@ -2,10 +2,15 @@ package com.isranascimento.challengealpha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.isranascimento.challengealpha.ui.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.root_container, HomeFragment())
+            .commit()
     }
 }
---
+++ android/app/src/main/java/com/isranascimento/challengealpha/ui/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.isranascimento.challengealpha.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.isranascimento.challengealpha.databinding.HomeFragmentBinding
+
+class HomeFragment: Fragment() {
+    private lateinit var binding: HomeFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = HomeFragmentBinding.inflate(inflater, container, false)
+            .also {
+                binding = it
+            }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        activity?.title = "Olá mundo"
+    }
+}
