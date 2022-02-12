fdb8611576abe5f0a463b544814ac4ed8bee6ca7
Israel <israel.nascimento@b2wdigital.com>
2022-02-12 15:54:29 -0300

    wip: recyclerview layout

--- android/app/src/main/java/com/isranascimento/challengealpha/ui/HomeFragment.kt
+++ android/app/src/main/java/com/isranascimento/challengealpha/ui/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.isranascimento.challengealpha.R
 import com.isranascimento.challengealpha.databinding.HomeFragmentBinding
+import com.isranascimento.hotelslist.ui.viewmodels.fragment.HotelListFragment
 
 class HomeFragment: Fragment() {
     private lateinit var binding: HomeFragmentBinding
@@ -22,5 +23,9 @@ class HomeFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         activity?.title = getString(R.string.app_name)
+        childFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container, HotelListFragment())
+            .commit()
     }
 }
--- android/hotels-list/src/main/java/com/isranascimento/hotelslist/ui/viewmodels/fragment/HotelListFragment.kt
+++ android/hotels-list/src/main/java/com/isranascimento/hotelslist/ui/viewmodels/fragment/HotelListFragment.kt
@@ -23,5 +23,6 @@ class HotelListFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
 }
