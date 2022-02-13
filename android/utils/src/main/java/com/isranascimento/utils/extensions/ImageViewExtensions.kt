3fa5a1de270dc644bce9fca9bf8a2e47e31cd357
Israel <israel.nascimento@b2wdigital.com>
2022-02-13 17:59:53 -0300

    feat: add error image

--- android/utils/src/main/java/com/isranascimento/utils/extensions/ImageViewExtensions.kt
+++ android/utils/src/main/java/com/isranascimento/utils/extensions/ImageViewExtensions.kt
@@ -1,13 +1,23 @@
 package com.isranascimento.utils.extensions
 
 import android.widget.ImageView
+import com.isranascimento.utils.R
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
+import java.lang.Exception
 
 fun ImageView.load(url: String?) {
     if(url?.isNotBlank() == true) {
         Picasso
             .get()
             .load(url)
-            .into(this)
+            .error(R.drawable.ic_image_error)
+            .into(this, object: Callback {
+                override fun onSuccess() {}
+
+                override fun onError(e: Exception?) {
+                    this@load.scaleType = ImageView.ScaleType.CENTER
+                }
+            })
     }
 }
