53307eec4f9b4669d6f7e5a22b460c59c6d8e22e
Israel <israel.nascimento@b2wdigital.com>
2022-02-14 19:57:40 -0300

    feat: adapter

--- android/hotels/src/main/java/com/isranascimento/hotels/ui/adapter/detail/HotelDetailGalleryAdapter.kt
+++ android/hotels/src/main/java/com/isranascimento/hotels/ui/adapter/detail/HotelDetailGalleryAdapter.kt
@@ -1,3 +1,33 @@
 package com.isranascimento.hotels.ui.adapter.detail
 
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.isranascimento.hotels.databinding.HotelDetailGalleryHolderBinding
+
+class HotelDetailGalleryAdapter(
+    private val gallery: List<String>
+): RecyclerView.Adapter<HotelDetailGalleryItemHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): HotelDetailGalleryItemHolder {
+        return HotelDetailGalleryItemHolder(
+            HotelDetailGalleryHolderBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: HotelDetailGalleryItemHolder, position: Int) {
+        holder.bind(gallery[position])
+    }
+
+    override fun getItemCount(): Int {
+        return gallery.count()
+    }
+}
---
+++ android/hotels/src/test/java/com/isranascimento/hotels/ui/adapter/detail/HotelDetailGalleryAdapterTest.kt
@@ -0,0 +1,55 @@
+package com.isranascimento.hotels.ui.adapter.detail
+
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.test.runner.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.isranascimento.androidtestutils.TestContextProvider
+import com.isranascimento.androidtestutils.TestContextProvider.context
+import com.isranascimento.hotels.databinding.HotelListCardItemBinding
+import com.isranascimento.hotels.ui.adapter.holder.BaseHotelListHolder
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Before
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class HotelDetailGalleryAdapterTest {
+
+    private lateinit var sut: HotelDetailGalleryAdapter
+    private lateinit var viewGroup: FrameLayout
+    @Before
+    fun setup() {
+        sut = HotelDetailGalleryAdapter(listOf("String"))
+        viewGroup = FrameLayout(context())
+    }
+
+    @Test
+    fun `WHEN onCreateViewHolder is called THEN it returns HotelDetailGalleryItemHolder`() {
+        val holder = sut.onCreateViewHolder(viewGroup, 0)
+
+        assertThat(holder).isInstanceOf(HotelDetailGalleryItemHolder::class.java)
+    }
+
+    @Test
+    fun `WHEN onBindViewHolder is called THEN it calls HotelDetailGalleryItemHolder_bind`() {
+        val viewHolderMockk = mockk<HotelDetailGalleryItemHolder>()
+        every { viewHolderMockk.bind(any()) } just Runs
+
+        sut.onBindViewHolder(viewHolderMockk, 0)
+        verify {
+            viewHolderMockk.bind("String")
+        }
+    }
+
+    @Test
+    fun `WHEN getItemCount is called THEN it returns correctly`() {
+
+        assertThat(sut.itemCount).isEqualTo(1)
+    }
+}
