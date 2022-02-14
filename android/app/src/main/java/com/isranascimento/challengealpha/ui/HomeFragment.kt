02d2582040e81a40efc27b23ac00c6badf11183f
Israel <israel.nascimento@b2wdigital.com>
2022-02-14 18:50:20 -0300

    feat: adding toolbar dinamically

--- android/app/src/main/java/com/isranascimento/challengealpha/ui/HomeFragment.kt
+++ android/app/src/main/java/com/isranascimento/challengealpha/ui/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.isranascimento.challengealpha.R
 import com.isranascimento.challengealpha.databinding.HomeFragmentBinding
@@ -22,10 +23,16 @@ class HomeFragment: Fragment() {
             }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        activity?.title = getString(R.string.app_name)
+        setupHomeToolbar()
         childFragmentManager
             .beginTransaction()
             .add(R.id.fragment_container, HotelListFragment())
             .commit()
+    }
+
+    private fun setupHomeToolbar() {
+        binding.root.findViewById<Toolbar>(R.id.toolbar).apply {
+            this.title = getString(R.string.app_name)
+        }
     }
 }
