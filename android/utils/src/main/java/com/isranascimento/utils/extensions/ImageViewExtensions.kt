4b3ddb799cd8fa08d6444a4da377307a67d43570
Israel <israel.nascimento@b2wdigital.com>
2022-02-15 11:48:40 -0300

    chore: change image load to coil

--- android/utils/src/main/java/com/isranascimento/utils/extensions/ImageViewExtensions.kt
+++ android/utils/src/main/java/com/isranascimento/utils/extensions/ImageViewExtensions.kt
@@ -1,6 +1,7 @@
 package com.isranascimento.utils.extensions
 
 import android.widget.ImageView
+import coil.load
 import com.isranascimento.utils.R
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
@@ -8,18 +9,10 @@ import java.lang.Exception
 
 fun ImageView.load(url: String?, scaleType: ImageView.ScaleType = ImageView.ScaleType.CENTER_CROP) {
     if(url?.isNotBlank() == true) {
-        Picasso
-            .get()
-            .load(url)
-            .error(R.drawable.ic_image_error)
-            .into(this, object: Callback {
-                override fun onSuccess() {
-                    this@load.scaleType = scaleType
-                }
-
-                override fun onError(e: Exception?) {
-                    this@load.scaleType = ImageView.ScaleType.CENTER
-                }
-            })
+        this.scaleType = scaleType
+        this.load(url) {
+            crossfade(true)
+            this.error(R.drawable.ic_image_error)
+        }
     }
 }
