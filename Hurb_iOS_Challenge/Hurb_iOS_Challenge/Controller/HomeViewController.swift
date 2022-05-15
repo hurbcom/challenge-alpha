087fb691d3d7ef92d287817a4695edfcf77892e4
Gáudio Uchôa Ney <gaudio.ney@gmail.com>
2022-05-15 16:30:15 -0300

    :construction: Set the project to ViewCode and organize the basic structure

---
+++ Hurb_iOS_Challenge/Hurb_iOS_Challenge/Controller/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  Hurb_iOS_Challenge
+//
+//  Created by Gáudio Ney on 12/05/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Helper Methods
+}
+
---
+++ Hurb_iOS_Challenge/Hurb_iOS_Challenge/Helper/CodeView.swift
@@ -0,0 +1,24 @@
+//
+//  CodeView.swift
+//  Hurb_iOS_Challenge
+//
+//  Created by Gáudio Ney on 15/05/22.
+//
+
+/// Protocol that set the necessary functions to build UIViews Hierarchy,  Constraints and Additional Configurations.
+protocol CodeView {
+    func buildViewHierarchy()
+    func setupConstraints()
+    func setupAdditionalConfiguration()
+    func setupView()
+}
+
+extension CodeView {
+    func setupView() {
+        buildViewHierarchy()
+        setupConstraints()
+        setupAdditionalConfiguration()
+    }
+    
+    func setupAdditionalConfiguration() {}
+}
