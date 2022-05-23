90eee757be55a87965644e0395142228aa40bdf9
Gáudio Uchôa Ney <gaudio.ney@gmail.com>
2022-05-22 21:39:25 -0300

    :construction: Add the fetchHotels Networking Service

---
+++ Hurb_iOS_Challenge/Hurb_iOS_Challenge/Constants/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Hurb_iOS_Challenge
+//
+//  Created by Gáudio Ney on 22/05/22.
+//
+
+import Foundation
+
+
+struct Constants {
+    // MARK: - JSON Files Names Constant
+    struct JSONFilesNames {
+        static let HOTEL_JSON_FILE_NAME = "hotel"
+        static let PACKAGE_JSON_FILE_NAME = "package"
+        static let JSON_TYPE = "json"
+    }
+}
