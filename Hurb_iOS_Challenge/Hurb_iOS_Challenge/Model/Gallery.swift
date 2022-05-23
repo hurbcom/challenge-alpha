b581d0b9b11bfd1802a23e32cc28d1096b0b9799
Gáudio Uchôa Ney <gaudio.ney@gmail.com>
2022-05-22 21:37:52 -0300

    :clown_face: Mocking the Models from Hotel JSON response

---
+++ Hurb_iOS_Challenge/Hurb_iOS_Challenge/Model/Gallery.swift
@@ -0,0 +1,19 @@
+//
+//  Gallery.swift
+//  Hurb_iOS_Challenge
+//
+//  Created by Gáudio Ney on 22/05/22.
+//
+
+import Foundation
+
+// MARK: - Gallery
+struct Gallery: Codable {
+    let galleryDescription: String?
+    let url: String?
+
+    enum CodingKeys: String, CodingKey {
+        case galleryDescription = "description"
+        case url
+    }
+}
---
+++ Hurb_iOS_Challenge/Hurb_iOS_Challenge/Model/Result.swift
@@ -0,0 +1,60 @@
+//
+//  Result.swift
+//  Hurb_iOS_Challenge
+//
+//  Created by Gáudio Ney on 22/05/22.
+//
+
+import Foundation
+
+// MARK: - Result
+struct HotelResult: Codable {
+    let sku: String?
+    let isHotel: Bool?
+    let category: ResultCategory?
+    let smallDescription: String?
+    let amenities: [ResultAmenity]?
+    let id: String?
+    let price: ResultPrice?
+    let huFreeCancellation: Bool?
+    let image: String?
+    let name: String?
+    let url: String?
+    let resultDescription: String?
+    let stars: Int?
+    let gallery: [Gallery]?
+    let address: Address?
+    let tags: [String]?
+    let quantityDescriptors: QuantityDescriptors?
+    let featuredItem: FeaturedItem?
+
+    enum CodingKeys: String, CodingKey {
+        case sku, isHotel, category, smallDescription, amenities, id, price
+        case huFreeCancellation = "hu_free_cancellation"
+        case image, name, url
+        case resultDescription = "description"
+        case stars, gallery, address, tags, quantityDescriptors, featuredItem
+    }
+}
+
+// MARK: - ResultAmenity
+struct ResultAmenity: Codable {
+    let name: String?
+    let category: AmenityCategory?
+}
+
+// MARK: - ResultPrice
+struct ResultPrice: Codable {
+    let currency, currencyOriginal: Currency?
+    let currentPrice, oldPrice: Double?
+    let sku: String?
+    let originalAmountPerDay, amountPerDay, amount: Double?
+
+    enum CodingKeys: String, CodingKey {
+        case currency
+        case currencyOriginal = "currency_original"
+        case currentPrice = "current_price"
+        case oldPrice = "old_price"
+        case sku, originalAmountPerDay, amountPerDay, amount
+    }
+}
