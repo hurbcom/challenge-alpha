8a94b776dbc78c4d5251683b6df12bdbe7a06444
Gáudio Uchôa Ney <gaudio.ney@gmail.com>
2022-06-04 12:41:39 -0300

    :lipstick: Add the default view message to user at Favorites View

--- Hurb_iOS_Challenge/Hurb_iOS_Challenge/Controller/FavoritesViewController.swift
+++ Hurb_iOS_Challenge/Hurb_iOS_Challenge/Controller/FavoritesViewController.swift
@@ -9,12 +9,43 @@ import UIKit
 
 class FavoritesViewController: UIViewController {
     // MARK: - Properties
+    private let comingSoonFavoritesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Em breve, a tela de `Favoritos` estará disponível.\n✌︎"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.textColor = .darkGray
+        label.tintColor = .darkGray
+        label.numberOfLines = 3
+        return label
+    }()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
 
     }
+}
+
+// MARK: - CodeView
+extension FavoritesViewController: CodeView {
+    func buildViewHierarchy() {
+        view.addSubview(comingSoonFavoritesLabel)
+    }
     
-    // MARK: - Helper Methods
+    func setupConstraints() {
+        comingSoonFavoritesLabel.anchor(leading: view.leadingAnchor, trailling: view.trailingAnchor)
+        comingSoonFavoritesLabel.centerY(inView: view)
+    }
+    
+    func setupAdditionalConfiguration() {
+        /// Configure View.
+        view.backgroundColor = .lightGray
+        
+        /// Configure NavigationBar.
+        title = "Favoritos"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+    }
 }
