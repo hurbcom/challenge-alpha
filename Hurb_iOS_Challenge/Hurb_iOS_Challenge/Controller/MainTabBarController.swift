8c5c97c27dda011d18ae211a269ecdb6ba479a03
Gáudio Uchôa Ney <gaudio.ney@gmail.com>
2022-06-04 11:53:53 -0300

    :fire: Remove unnecessary code from tabBar layout and update it

--- Hurb_iOS_Challenge/Hurb_iOS_Challenge/Controller/MainTabBarController.swift
+++ Hurb_iOS_Challenge/Hurb_iOS_Challenge/Controller/MainTabBarController.swift
@@ -14,35 +14,10 @@ class MainTabBarController: UITabBarController {
     let favoritesViewController = FavoritesViewController()
     
     // MARK: - Lifecycle
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        if #available(iOS 13.0, *) {
-            let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
-            lazy var statusBarHeight = window?.windowScene?.statusBarManager?.statusBarFrame.size.height ?? 20
-            
-            let statusbarView = UIView()
-            statusbarView.backgroundColor = UIColor.white
-            view.addSubview(statusbarView)
-          
-            statusbarView.translatesAutoresizingMaskIntoConstraints = false
-            statusbarView.heightAnchor
-                .constraint(equalToConstant: statusBarHeight).isActive = true
-            statusbarView.widthAnchor
-                .constraint(equalTo: view.widthAnchor, multiplier: 1.0).isActive = true
-            statusbarView.topAnchor
-                .constraint(equalTo: view.topAnchor).isActive = true
-            statusbarView.centerXAnchor
-                .constraint(equalTo: view.centerXAnchor).isActive = true
-          
-        } else {
-            let statusBar = UIApplication.shared.value(forKeyPath: "statusBarWindow.statusBar") as? UIView
-            statusBar?.backgroundColor = UIColor.white
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureMainTabBarControllers()
+        view.backgroundColor = .white
     }
     
     // MARK: - Helper Methods
