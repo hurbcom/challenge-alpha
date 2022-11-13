7ae644aa38f529a0228798bf82a94f19be89609d
Helio Vieira <helio.engenharia@gmail.com>
2022-11-13 20:59:34 -0300

    🧩: Implemented View Layer of Scene Hotel.

--- Projects/Challenge_iOS/Challenge_iOS/Scenes/Hotel/HotelViewController.swift
+++ Projects/Challenge_iOS/Challenge_iOS/Scenes/Hotel/HotelViewController.swift
@@ -7,23 +7,78 @@
 
 import UIKit
 
-class HotelViewController: UIViewController {
+class HotelViewController: BaseViewController {
     // MARK: Properties
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var viewSearchSuggestions: SuggestionsView = SuggestionsView.fromNib()
     
     // MARK: Outlets
+    @IBOutlet weak var tableView: UITableView!
     
     // MARK: Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
+        bindEvents()
+        setupUI()
     }
     
     // MARK: Actions
     
     // MARK: BindEvents
+    private func bindEvents() {
+        
+    }
     
     // MARK: Methods
-    
-    // MARK: Updates
-    
+        
     // MARK: Setup
+    private func setupUI() {
+        setupSearchController()
+        setupSugestionView()
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        
+    }
+    
+    private func setupSearchController() {
+        navigationItem.searchController = self.searchController
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Pesquisar..."
+        searchController.searchBar.delegate = self
+    }
+    
+    private func setupSugestionView() {
+        viewSearchSuggestions.isHidden = true
+        viewSearchSuggestions.frame = view.bounds
+        view.addSubview(viewSearchSuggestions)
+        viewSearchSuggestions.bringSubviewToFront(view)
+    }
+}
+
+// MARK: Extensions
+extension HotelViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        viewModel.getSuggestionsFrom(text: searchText)
+    }
+    
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        viewSearchSuggestions.isHidden = false
+        return true
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        viewSearchSuggestions.isHidden = true
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.becomeFirstResponder()
+        showLoading()
+        viewModel.fetchHotelFrom(query: searchBar.text ?? "")
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.becomeFirstResponder()
+    }
 }
