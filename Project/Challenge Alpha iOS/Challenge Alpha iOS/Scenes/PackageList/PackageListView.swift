9bbd5fd3f8c0a3d1e70a52ee1a9e3f2f394ab8b2
Yuri Strack <yuri.strack@gmail.com>
2023-01-17 12:29:26 -0300

    ♻️ Move PackageCardView to UIComponents folder

--- Project/Challenge Alpha iOS/Challenge Alpha iOS/Scenes/PackageList/PackageListView.swift
+++ Project/Challenge Alpha iOS/Challenge Alpha iOS/Scenes/PackageList/PackageListView.swift
@@ -15,7 +15,7 @@ struct PackageListView: View {
         ScrollView {
             VStack(alignment: .leading, spacing: 12) {
                 ForEach(0..<viewModel.packages.count, id: \.self) { index in
-                    PackageView(
+                    PackageCardView(
                         city: viewModel.packages[index].address?.city ?? "",
                         country: viewModel.packages[index].address?.country ?? "",
                         name: viewModel.packages[index].name ?? "",
@@ -25,7 +25,7 @@ struct PackageListView: View {
                         originalAmount: viewModel.packages[index].price?.originalAmount ?? 0.0,
                         tags: viewModel.getTagsForPackage(at: index),
                         imagesURL: viewModel.getImagesURLForPackage(at: index),
-                        action: {print("Cliquei")}
+                        action: {}
                     )
                 }
             }
@@ -34,91 +34,6 @@ struct PackageListView: View {
         .loading($viewModel.showLoading)
         .onAppear {
             self.viewModel.getPackages()
-        }
-    }
-    
-    @ViewBuilder
-    func PackageView(
-        city: String,
-        country: String,
-        name: String,
-        duration: Int,
-        maxPeople: Int,
-        amount: Double,
-        originalAmount: Double,
-        tags: [String],
-        imagesURL: [String],
-        action: @escaping (() -> Void)
-    ) -> some View {
-        VStack(alignment: .leading, spacing: 0) {
-            ZStack(alignment: .topLeading) {
-                ImageCarousel(imagesURL: imagesURL)
-                    .cornerRadius(8, corners: .topLeft)
-                    .cornerRadius(8, corners: .topRight)
-                
-                HStack(spacing: 8) {
-                    ForEach(tags, id: \.self) { tag in
-                        HUTagView(title: tag, textColor: Color.white, backgroundColor: UIConstants.COLOR.hurbOrange)
-                    }
-                }
-                .padding()
-            }
-            
-            HStack {
-                VStack(alignment: .leading, spacing: 12) {
-                    VStack(alignment: .leading, spacing: 8) {
-                        // Location
-                        Text("\(city), \(country)")
-                            .font(.system(size: 14))
-                            .foregroundColor(UIConstants.COLOR.hurbDarkGray)
-                        
-                        // Package name
-                        Text("\(name)")
-                            .font(.system(size: 16, weight: .semibold))
-                            .foregroundColor(.black)
-                            .lineLimit(2)
-                    }
-                    
-                    // Quantity description
-                    HStack(spacing: 16) {
-                        HULabel(imageName: "sun.max", title: "\(duration) diárias", fillColor: UIConstants.COLOR.hurbDarkGray)
-                        HULabel(imageName: "person", title: "\(maxPeople) \(maxPeople > 1 ? "pessoas" : "pessoa")", fillColor: UIConstants.COLOR.hurbDarkGray)
-                    }
-                    
-                    VStack(alignment: .leading, spacing: 2) {
-                        // Original amount
-                        Text("A partir de ")
-                            .font(.system(size: 14))
-                            .foregroundColor(UIConstants.COLOR.hurbDarkGray)
-                        +
-                        Text("\(originalAmount != amount ? "R$ \(String(format: "%.0f", amount / 100))" : "")")
-                            .strikethrough()
-                            .font(.system(size: 14))
-                            .foregroundColor(UIConstants.COLOR.hurbDarkGray)
-                        
-                        // Amount
-                        Text("R$ \(String(format: "%.0f", originalAmount / 100))")
-                            .font(.system(size: 18, weight: .bold))
-                            .foregroundColor(originalAmount != amount ? UIConstants.COLOR.hurbOrange : .black)
-                        +
-                        Text(" até 12x no cartão")
-                            .font(.system(size: 14))
-                            .foregroundColor(UIConstants.COLOR.hurbDarkGray)
-                    }
-                }
-                Spacer()
-            }
-            .frame(maxWidth: .infinity)
-            .padding()
-            .background(
-                Color.white
-                    .cornerRadius(8, corners: .bottomLeft)
-                    .cornerRadius(8, corners: .bottomRight)
-                    .shadow(color: UIConstants.COLOR.hurbGray.opacity(0.4), radius: 5.0)
-            )
-        }
-        .onTapGesture {
-            action()
         }
     }
 }
---
+++ Project/Challenge Alpha iOS/Challenge Alpha iOS/UIComponents/Cards/PackageCardView.swift
@@ -0,0 +1,130 @@
+//
+//  PackageCardView.swift
+//  Challenge Alpha iOS
+//
+//  Created by Yuri Strack on 17/01/23.
+//
+
+import SwiftUI
+
+struct PackageCardView: View {
+    
+    // MARK: - Properties
+    var city: String
+    var country: String
+    var name: String
+    var duration: Int
+    var maxPeople: Int
+    var amount: Double
+    var originalAmount: Double
+    var tags: [String]
+    var imagesURL: [String]
+    var action: (() -> Void)
+    
+    var body: some View {
+        self.content
+            .onTapGesture {
+                action()
+            }
+    }
+    
+    var content: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            // Image Carousel with Tags
+            self.imageHeader
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 12) {
+                    // Pacakge Location + Name
+                    self.packageGeneralInfo
+                    
+                    // Quantity description
+                    self.packageQuantityInfo
+                    
+                    // Price
+                    self.packagePriceInfo
+                }
+                
+                Spacer()
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(
+                self.cardBackground
+            )
+        }
+    }
+    
+    // MARK: - View Components
+    /// Image Carousel with tags on top
+    var imageHeader: some View {
+        ZStack(alignment: .topLeading) {
+            ImageCarousel(imagesURL: imagesURL)
+                .cornerRadius(8, corners: .topLeft)
+                .cornerRadius(8, corners: .topRight)
+            
+            HStack(spacing: 8) {
+                ForEach(tags, id: \.self) { tag in
+                    HUTagView(title: tag, textColor: Color.white, backgroundColor: UIConstants.COLOR.hurbOrange)
+                }
+            }
+            .padding()
+        }
+    }
+    
+    /// Package Location and Name
+    var packageGeneralInfo: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            // Location
+            Text("\(city), \(country)")
+                .font(.system(size: 14))
+                .foregroundColor(UIConstants.COLOR.hurbDarkGray)
+            
+            // Package name
+            Text("\(name)")
+                .font(.system(size: 16, weight: .semibold))
+                .foregroundColor(.black)
+                .lineLimit(2)
+        }
+    }
+    
+    /// Quantity Descriptors
+    var packageQuantityInfo: some View {
+        HStack(spacing: 16) {
+            HULabel(imageName: "sun.max", title: "\(duration) diárias", fillColor: UIConstants.COLOR.hurbDarkGray)
+            HULabel(imageName: "person", title: "\(maxPeople) \(maxPeople > 1 ? "pessoas" : "pessoa")", fillColor: UIConstants.COLOR.hurbDarkGray)
+        }
+    }
+    
+    /// Price information (amount + general amount)
+    var packagePriceInfo: some View {
+        VStack(alignment: .leading, spacing: 2) {
+            // Original amount
+            Text("A partir de ")
+                .font(.system(size: 14))
+                .foregroundColor(UIConstants.COLOR.hurbDarkGray)
+            +
+            Text("\(originalAmount != amount ? "R$ \(String(format: "%.0f", amount / 100))" : "")")
+                .strikethrough()
+                .font(.system(size: 14))
+                .foregroundColor(UIConstants.COLOR.hurbDarkGray)
+            
+            // Amount
+            Text("R$ \(String(format: "%.0f", originalAmount / 100))")
+                .font(.system(size: 18, weight: .bold))
+                .foregroundColor(originalAmount != amount ? UIConstants.COLOR.hurbOrange : .black)
+            +
+            Text(" até 12x no cartão")
+                .font(.system(size: 14))
+                .foregroundColor(UIConstants.COLOR.hurbDarkGray)
+        }
+    }
+    
+    /// White card background with shadow
+    var cardBackground: some View {
+        Color.white
+            .cornerRadius(8, corners: .bottomLeft)
+            .cornerRadius(8, corners: .bottomRight)
+            .shadow(color: UIConstants.COLOR.hurbGray.opacity(0.4), radius: 5.0)
+    }
+}
