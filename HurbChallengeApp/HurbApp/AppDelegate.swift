349a49c15df33e7213349d0224bf608460f40f75
Fabio Takahashi <fab.takahashi@gmail.com>
2023-01-17 13:16:51 -0300

    [ADD] Added HurbApp project stantard configuration

---
+++ HurbChallengeApp/HurbApp/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  HurbApp
+//
+//  Created by Fabio Takahashi on 17/01/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
