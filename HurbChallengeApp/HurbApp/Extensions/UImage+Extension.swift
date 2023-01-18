d44ca1abf61375c13b9701f619c3ee89b533c69f
Fabio Takahashi <fab.takahashi@gmail.com>
2023-01-18 12:47:21 -0300

    [ADD] Created UIImage extension file

---
+++ HurbChallengeApp/HurbApp/Extensions/UImage+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UImage+Extension.swift
+//  HurbApp
+//
+//  Created by Fabio Takahashi on 18/01/23.
+//
+
+import UIKit
+
+extension UIImage {
+
+    // MARK: - SF Symbols
+    static let searchIcon = UIImage(systemName: "magnifyingglass.circle")
+
+}
