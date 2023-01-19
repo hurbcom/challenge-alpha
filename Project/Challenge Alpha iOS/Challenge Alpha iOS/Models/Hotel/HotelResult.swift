ba8d460b120cbd53f950ee36e59a5eaac9f50038
Yuri Strack <yuri.strack@gmail.com>
2023-01-19 15:43:39 -0300

    🚧 Add Hotel Models

---
+++ Project/Challenge Alpha iOS/Challenge Alpha iOS/Models/Hotel/HotelResult.swift
@@ -0,0 +1,96 @@
+//
+//  HotelResult.swift
+//  Challenge Alpha iOS
+//
+//  Created by Yuri Strack on 19/01/23.
+//
+
+import Foundation
+
+// MARK: - Hotel Result
+struct HotelResult: Codable {
+    var id: String?
+    var sku: String?
+    var name: String?
+    var url: String?
+    var stars: Int?
+    var isAvailable: Bool?
+    var huFreeCancellation: Bool?
+    var description: String?
+    var price: Price?
+    var address: Address?
+    var tags: [Tag]?
+    var gallery: [GalleryItem]?
+    var amenities: [Amenity]?
+    
+    // MARK: - Getters
+    func getCity() -> String {
+        return self.address?.city ?? ""
+    }
+    
+    func getCountry() -> String {
+        return self.address?.country ?? ""
+    }
+    
+    func getName() -> String {
+        return self.name ?? ""
+    }
+    
+    func getDescription() -> String {
+        return self.description ?? ""
+    }
+    
+    func getAmount() -> Double {
+        return self.price?.amount ?? 0.0
+    }
+    
+    func getFormattedAmount() -> String {
+        guard let currency = self.price?.currency else {
+            return ""
+        }
+        return self.getAmount().formatCurrency(currency)
+    }
+    
+    func getOriginalAmount() -> Double {
+        return self.price?.originalAmount ?? 0.0
+    }
+    
+    func getFormattedOriginalAmount() -> String {
+        guard let currency = self.price?.currency else {
+            return ""
+        }
+        return self.getFixedOriginalAmount().formatCurrency(currency)
+    }
+    
+    func getImagesURL() -> [String] {
+        guard let gallery = self.gallery else {
+            return []
+        }
+        
+        let urls = gallery.map { item -> String in
+            return item.url ?? ""
+        }
+        
+        return urls
+    }
+    
+    func getAmenitiesName() -> [String] {
+        guard let amenities = self.amenities else {
+            return []
+        }
+        
+        let amenitiesName = amenities.map { item -> String in
+            return item.name ?? ""
+        }
+        
+        return amenitiesName
+    }
+    
+    func getLatitude() -> Double {
+        return self.address?.geolocation?.latitude ?? 0.0
+    }
+    
+    func getLongitude() -> Double {
+        return self.address?.geolocation?.longitude ?? 0.0
+    }
+}
---
+++ Project/Challenge Alpha iOS/Challenge Alpha iOS/Models/Hotel/SearchHotel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchHotel.swift
+//  Challenge Alpha iOS
+//
+//  Created by Yuri Strack on 19/01/23.
+//
+
+import Foundation
+
+// MARK: - SearchHotel
+struct SearchHotel: Codable {
+    var results: [HotelResult]
+}
