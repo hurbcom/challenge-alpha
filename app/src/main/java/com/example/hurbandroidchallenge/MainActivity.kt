c53e64ebdcd711e01b3c7d151912e4ef37da3a35
Pedro Resende <pedro.resende@inchurch.com.br>
2023-04-05 20:51:18 -0300

    setup app theme

--- app/src/main/java/com/example/hurbandroidchallenge/MainActivity.kt
+++ app/src/main/java/com/example/hurbandroidchallenge/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.hurbandroidchallenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.hurbandroidchallenge.ui.theme.HurbAndroidChallengeTheme
+import com.example.hurbandroidchallenge.presentation.compose.theme.HurbAndroidChallengeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
