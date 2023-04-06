a3f5370cfa1db8f69e7c4736fb079f884169b392
Pedro Resende <pedro.resende@inchurch.com.br>
2023-04-05 21:25:13 -0300

    fix: splash background color; dummy content in home

--- app/src/main/java/com/example/hurbandroidchallenge/MainActivity.kt
+++ app/src/main/java/com/example/hurbandroidchallenge/MainActivity.kt
@@ -3,6 +3,12 @@ package com.example.hurbandroidchallenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.example.hurbandroidchallenge.presentation.compose.theme.HurbAndroidChallengeTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +16,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HurbAndroidChallengeTheme {
-
+                Surface {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Text(modifier = Modifier.align(Alignment.Center), text = "Hello world")
+                    }
+                }
             }
         }
     }
