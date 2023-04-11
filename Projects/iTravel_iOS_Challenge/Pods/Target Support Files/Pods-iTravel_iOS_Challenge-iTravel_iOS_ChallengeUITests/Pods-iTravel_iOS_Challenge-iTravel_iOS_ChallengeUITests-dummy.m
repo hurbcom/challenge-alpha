07aeaa140ab1514c8315b3f52a62976087728a67
Yago Vanzan <villarvanzan@gmail.com>
2023-04-11 04:56:18 -0300

    Correção de bugs

--- Projects/iTravel_iOS_Challenge/Pods/Target Support Files/Pods-iTravel_iOS_Challenge-iTravel_iOS_ChallengeUITests/Pods-iTravel_iOS_Challenge-iTravel_iOS_ChallengeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iTravel_iOS_Challenge_iTravel_iOS_ChallengeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_iTravel_iOS_Challenge_iTravel_iOS_ChallengeUITests
-@end
--- Projects/iTravel_iOS_Challenge/Pods/Target Support Files/Pods-iTravel_iOS_Challenge/Pods-iTravel_iOS_Challenge-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iTravel_iOS_Challenge : NSObject
-@end
-@implementation PodsDummy_Pods_iTravel_iOS_Challenge
-@end
